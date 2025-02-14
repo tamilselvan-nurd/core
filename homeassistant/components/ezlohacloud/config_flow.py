c45f74c58729a00b5f0ee2ee1bd16f6e517c3cef
Fahad Khan <fk.fahad96@gmail.com>
2025-02-14 19:04:54 +0500

    feat: add ui forms

--- homeassistant/components/ezlohacloud/config_flow.py
+++ homeassistant/components/ezlohacloud/config_flow.py
@@ -1,44 +1,27 @@
-"""Config flow for Ezlo HA Cloud."""
-
-# import my_pypi_dependency
-
-# from homeassistant.core import HomeAssistant
-# from homeassistant.helpers import config_entry_flow
-
-# from .const import DOMAIN
-
-
-# async def _async_has_devices(hass: HomeAssistant) -> bool:
-#     """Return if there are devices that can be discovered."""
-#     devices = await hass.async_add_executor_job(my_pypi_dependency.discover)
-#     return len(devices) > 0
-
-
-# config_entry_flow.register_discovery_flow(DOMAIN, "Ezlo HA Cloud", _async_has_devices)
-
 import voluptuous as vol
-
+import logging
 from homeassistant import config_entries
+from homeassistant.core import callback
 
 from .const import DOMAIN
+from .api import authenticate
+from .options_flow import EzloOptionsFlowHandler  # Import options flow
+
+_LOGGER = logging.getLogger(__name__)
 
 
-class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Example config flow."""
+class EzloConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Config flow for Ezlo HA Cloud."""
 
-    # The schema version of the entries that it creates
-    # Home Assistant will call your migrate method if the version changes
     VERSION = 1
     MINOR_VERSION = 1
 
     async def async_step_user(self, user_input=None):
-        """Show user setup form."""
+        """Initial setup form."""
         errors = {}
 
         if user_input is not None:
-            # Validate the input and store data
             try:
-                # Custom validation logic here
                 await self.async_set_unique_id("user_authentication")
                 self._abort_if_unique_id_configured()
                 return self.async_create_entry(title="Ezlo HA Cloud", data=user_input)
@@ -47,27 +30,22 @@ class ExampleConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             except Exception as e:  # noqa: BLE001
                 errors["base"] = str(e)
 
-        # Specify items in the order they are to be displayed in the UI
-        # data_schema = {
-        #     vol.Required("username"): str,
-        #     vol.Required("password"): str,
-        # }
-
-        data_schema = {
-            vol.Required("sni_host"): str,
-            vol.Required("sni_port"): int,
-            vol.Required("end_host"): str,
-            vol.Required("end_port"): int,
-            vol.Required("fernet_token"): str,
-        }
-
-        # await self.async_set_unique_id("ezlo_user_data")
-
-        return self.async_show_form(
-            step_id="user", data_schema=vol.Schema(data_schema), errors=errors
+        data_schema = vol.Schema(
+            {
+                vol.Required("sni_host"): str,
+                vol.Required("sni_port"): int,
+                vol.Required("end_host"): str,
+                vol.Required("end_port"): int,
+                vol.Required("fernet_token"): str,
+            }
         )
 
-    async def async_step_finish(self, user_input=None):  # noqa: D102
-        if not user_input:
-            return self.async_show_form(step_id="finish")
-        return self.async_create_entry(title="", data={})
+        return self.async_show_form(
+            step_id="user", data_schema=data_schema, errors=errors
+        )
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Enable the 'Configure' and 'Login' buttons in the UI."""
+        return EzloOptionsFlowHandler()
